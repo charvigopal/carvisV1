bda7c03da18cc24b62de881bcf8dc6dbfd32d1a9
Ishan Pakuwal <51416000+ishanpakuwal@users.noreply.github.com>
2022-04-03 14:08:46 -0400

    Add files via upload

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,16 +1,26 @@
 # speech_to_text.py
 import speech_recognition as sr
+import time
 
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-with mic as source:
-	audio = r.listen(source)
-	
+
 #filename = "Hello World.wav"
 
 # with sr.AudioFile(filename) as source:
 #     audio = r.listen(source)
+time_1 = time.time()
+data = ""
+time_interval = 10
+stopsignal = False
+while not stopsignal:
+	with mic as source:
+		audio = r.listen(source)
+	text = r.recognize_google(audio)
+	print(text)
+	data += text
+	time_2 = time.time()
+	if time_2 - time_1 >= time_interval:
+		stopsignal = True
 
-text = r.recognize_google(audio)
-print(text)
