dc3a6e812dc9306581fa9ef8df953e6a3683c426
charvigopal <charvig2021@gmail.com>
2022-04-06 02:49:44 -0400

    Add stopwords

--- audio_demo_prototype.py
+++ audio_demo_prototype.py
@@ -38,7 +38,7 @@ for i in range(len(data)):
 	else:
 		output[data[i]] += 1
 
-stopwords = ['to', 'the', 'a', 'from', 'I', 'that', 'me', 'do', 'and', 'is', 'my', 'for', 'of', 'it']
+stopwords = ['to', 'the', 'a', 'from', 'I', 'that', 'me', 'do', 'and', 'is', 'my', 'for', 'of', 'it', 'if', 'in', 'we', 'but', 'you']
 for word in stopwords:
 	try:
 		del output[word]
