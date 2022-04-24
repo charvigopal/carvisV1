d7cc2cb8fb052ceed283aabc6da32e51085f7a66
Ishan Pakuwal <51416000+ishanpakuwal@users.noreply.github.com>
2022-04-23 22:00:30 -0400

    Add files via upload

--- video_transcript.py
+++ video_transcript.py
@@ -233,7 +233,7 @@ for i in range(len(data)):
 	else:
 		output[data[i]] += 1
 
-stopwords = ['with', 'has', 'by', 'to', 'the', 'a', 'from', 'I', 'that', 'me', 'do', 'and', 'is', 'my', 'for', 'of', 'it', 'if', 'in', 'we', 'but', 'you', 'are', 'have', 'not', 'it\'s', 'I\'m', 'am', 'at', 'so', 'will', 'be', 'an', 'that']
+stopwords = ['with', 'has', 'by', 'to', 'the', 'a', 'from', 'on', 'that', 'been', 'do', 'and', 'is', 'for', 'of', 'it', 'if', 'in', 'which', 'but', 'are', 'have', 'not', 'it\'s', 'I\'m', 'am', 'at', 'so', 'will', 'be', 'an', 'that', 'can', 'this', 'can']
 for word in stopwords:
 	try:
 		del output[word]
