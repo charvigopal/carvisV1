fa3fc2310644ac4e83dc0d6b5f383ab607577119
Ishan Pakuwal <51416000+ishanpakuwal@users.noreply.github.com>
2022-04-29 21:52:06 -0400

    Add files via upload

--- video_transcript.py
+++ video_transcript.py
@@ -13,6 +13,8 @@ from datetime import datetime
 from scipy.io import wavfile
 import gtts
 from playsound import playsound
+import fitz
+from nltk.corpus import wordnet
 
 now = datetime.now()
 new_now = now.strftime("%H%M%S")
@@ -110,36 +112,54 @@ f.close()
 # f.close()
 
 
-
+filler_words = {'like','um', 'umm', 'totally', 'hmm', 'hmmm', 'literally', 'really', 'uh', 'actually', 'so', 'very', 'simply', 'slightly', 'basically', 'just', 'also'}
 
 import textwrap
 from fpdf import FPDF
 
 def text_to_pdf(text, filename):
-    a4_width_mm = 210
-    pt_to_mm = 0.35
-    fontsize_pt = 10
-    fontsize_mm = fontsize_pt * pt_to_mm
-    margin_bottom_mm = 10
-    character_width_mm = 7 * pt_to_mm
-    width_text = a4_width_mm / character_width_mm
+	a4_width_mm = 210
+	pt_to_mm = 0.35
+	fontsize_pt = 10
+	fontsize_mm = fontsize_pt * pt_to_mm
+	margin_bottom_mm = 10
+	character_width_mm = 7 * pt_to_mm
+	width_text = a4_width_mm / character_width_mm
 
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
-    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
-    pdf.add_page()
-    pdf.set_font(family='Courier', size=fontsize_pt)
-    splitted = text.split('\n')
+	pdf = FPDF(orientation='P', unit='mm', format='A4')
+	pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+	pdf.add_page()
+	pdf.set_font(family='Courier', size=fontsize_pt)
+	splitted = text.split('\n')
 
-    for line in splitted:
-        lines = textwrap.wrap(line, width_text)
+	for line in splitted:
+		lines = textwrap.wrap(line, width_text)
+		# print("This is a line", lines)
+		# print(type(lines))
+		# print(".......")
+		count = 0
+		if len(lines) == 0:
+			pdf.ln()
 
-        if len(lines) == 0:
-            pdf.ln()
+		for wrap in lines:
+		# 	count += 1
 
-        for wrap in lines:
-            pdf.cell(0, fontsize_mm, wrap, ln=1)
+			# print(wrap)
+			# print(wrap.split(' ')
+			# wrap_list = wrap.split('')
+			# for word_ in wrap_list:
+			# 	if word_ in filler_words:
 
-    pdf.output(filename, 'F')
+
+			# if count == 4:
+			# 	pdf.set_text_color(255,0,0)
+			# if count == 5:
+			# 	pdf.set_text_color(0, 0, 0)
+
+			# print("....")
+			pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+	pdf.output(filename, 'F')
 
 
 # input_filename = 'transcription.txt'
@@ -153,41 +173,45 @@ text_to_pdf(text, output_filename)
 
 
 
+
+
+
+
 def visualize(path: str):
-    # reading the audio file
-    raw = wave.open(path)  
-    # reads all the frames
-    # -1 indicates all or max frames
-    signal = raw.readframes(-1)
-    signal = np.frombuffer(signal, dtype ="int16")    
-    # gets the frame rate
-    f_rate = raw.getframerate()
-    # to Plot the x-axis in seconds
-    # you need get the frame rate
-    # and divide by size of your signal
-    # to create a Time Vector
-    # spaced linearly with the size
-    # of the audio file
-    time = np.linspace(
-        0, # start
-        len(signal) / f_rate,
-        num = len(signal)
-    ) 
+	# reading the audio file
+	raw = wave.open(path)  
+	# reads all the frames
+	# -1 indicates all or max frames
+	signal = raw.readframes(-1)
+	signal = np.frombuffer(signal, dtype ="int16")    
+	# gets the frame rate
+	f_rate = raw.getframerate()
+	# to Plot the x-axis in seconds
+	# you need get the frame rate
+	# and divide by size of your signal
+	# to create a Time Vector
+	# spaced linearly with the size
+	# of the audio file
+	time = np.linspace(
+		0, # start
+		len(signal) / f_rate,
+		num = len(signal)
+	) 
 
-    plt.figure(1)  
-    plt.title("User Presentation Audio Amplitude") 
+	plt.figure(1)  
+	plt.title("User Presentation Audio Amplitude") 
 
-    plt.xlabel("Time")
+	plt.xlabel("Time")
 
-    plt.plot(time, signal)
+	plt.plot(time, signal)
 
-    # plt.show()
-    plt.savefig('UserAmplitudePlot.pdf', bbox_inches='tight')
+	# plt.show()
+	plt.savefig('UserAmplitudePlot.pdf', bbox_inches='tight')
 
  
-    # Can also save
-    # the plot using
-    # plt.savefig('filename')
+	# Can also save
+	# the plot using
+	# plt.savefig('filename')
 
 # visualize("transcribed_speech.wav")
 visualize('demo.wav')
@@ -229,6 +253,10 @@ plt.savefig("SentimentPlot.pdf")
 
 
 
+
+
+
+
 output = {}
 freq_dict = Counter(data)
 for i in range(len(data)):
@@ -250,7 +278,7 @@ words_vals = [output[words_key] for words_key in words_keys]
 fig =  plt.figure(0, figsize = (10, 5))
 
 plt.bar(words_keys, words_vals, color ='maroon',
-        width = 0.4)
+		width = 0.4)
 
 plt.xlabel("Common spoken words")
 plt.ylabel("No. of times spoken")
@@ -267,7 +295,6 @@ plt.savefig("CommonWords.pdf")
 
 # ### Filler Words ### 
 
-filler_words = {'like','um', 'umm', 'totally', 'hmm', 'hmmm', 'literally', 'really', 'uh', 'actually', 'so', 'very', 'simply', 'slightly', 'basically', 'just'}
 filler_output = dict()
 freq_dict = Counter(data)
 for i in range(len(data)):
@@ -282,13 +309,33 @@ words_filler_vals = list(filler_output.values())
 fig =  plt.figure(2, figsize = (10, 5))
 
 plt.bar(words_filler_key, words_filler_vals, color ='blue',
-        width = 0.4)
+		width = 0.4)
 
 plt.xlabel("Filler words spoken")
 plt.ylabel("No. of times spoken")
 plt.title("Filler words used in Speech:")
 # plt.show()
 plt.savefig("FillerWords.pdf")
+
+
+
+
+open_transcription = fitz.open(output_filename)
+for each_page in open_transcription:
+	for filler_word in filler_words:
+		#Search
+		word_instances = each_page.searchFor(filler_word)
+		# print("word instances", word_instances)
+		#Highlight
+		for inst in word_instances:
+
+			highlight = each_page.addHighlightAnnot(inst)
+			highlight.setColors(stroke = fitz.utils.getColor('red'))
+			# highlight.setColors(colors= fitz.utils.getColor('red'))
+			highlight.update()
+
+open_transcription.save('FillerWordsTranscription.pdf')
+
 
 
 
@@ -306,7 +353,7 @@ words_long_vals = [longWordsOutput[words_key] for words_key in words_long_key]
 
 fig = plt.figure(4, figsize = (10, 5))
 plt.bar(words_long_key, words_long_vals, color ='purple',
-        width = 0.4)
+		width = 0.4)
 
 plt.xlabel("Long words spoken")
 plt.ylabel("No. of times spoken")
@@ -316,9 +363,26 @@ plt.savefig("LongWords.pdf")
 
 
 
+list_of_synonyms = []
+for each_word in words_long_key:
+	synonyms = [each_word+":"]
+	if len(each_word) > 8:
+		for syn in wordnet.synsets(each_word):
+			for i in syn.lemmas():
+			    synonyms.append(i.name())
+
+		list_of_synonyms.append(synonyms)
+print("List of synonyms", list_of_synonyms)
+
+
+
+
+
+
+
 
 from PyPDF2 import PdfFileMerger
-pdfs = ['CommonWords.pdf', 'LongWords.pdf', 'UserAmplitudePlot.pdf', 'SentimentPlot.pdf', 'FillerWords.pdf', output_filename]
+pdfs = [ output_filename, 'CommonWords.pdf', 'LongWords.pdf', 'UserAmplitudePlot.pdf', 'SentimentPlot.pdf', 'FillerWords.pdf','FillerWordsTranscription.pdf']
 merger = PdfFileMerger()
 for pdf in pdfs:
 	merger.append(pdf)
